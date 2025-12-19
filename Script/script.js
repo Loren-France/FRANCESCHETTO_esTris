e1e5aaa86359cc194fe1332e54687cdb9890fdd7
Loren-France <14529846@itisrossi.vi.it>
2025-12-19 18:32:02 +0100

    con inserimento di O e X

--- Script/script.js
+++ Script/script.js
@@ -1,33 +1,37 @@
+
 let griglia = [
-    ["", "", ""],
-    ["", "", ""],
-    ["", "", ""]
+  ["", "", ""],
+  ["", "", ""],
+  ["", "", ""]
 ];
-let t=1;
+let t = 1;
 
 function cliccato() {
-    let tabella="";
-
-    tabella+="<tbody>";
-    for (let i = 0; i < griglia.length; i++) {
-        tabella+="<tr>";
-        for (let j = 0; j < griglia[i].length; j++) {
-            griglia[i][j]=`<td><button onclick='click(${i},${j})' class='caselle' id='casella-${i}-${j}'></button></td>`;
-            tabella += griglia[i][j];
-        }
-        tabella+="</tr>";
+  let tabella = "<tbody>";
+  for (let i = 0; i < griglia.length; i++) {
+    tabella += "<tr>";
+    for (let j = 0; j < griglia[i].length; j++) {
+      tabella += `
+        <td>
+          <button onclick="clickCasella(${i},${j})" class="caselle" id="casella-${i}-${j}">${griglia[i][j]}</button></td>`;
     }
-    document.getElementById("griglia").innerHTML = tabella;
+    tabella += "</tr>";
+  }
+  tabella += "</tbody>";
+  document.getElementById("griglia").innerHTML = tabella;
 }
 
-function click() {
+function clickCasella(i, j) {
+  if (griglia[i][j] !== "") return;
 
-    if (t % 2 != 0) {
-        document.getElementById(`casella-${i}-${j}`).textContent = "X";
-        document.getElementById(`casella-${i}-${j}`).disabled = true;
-    } else {
-        document.getElementById(`casella-${i}-${j}`).textContent = "O";
-        document.getElementById(`casella-${i}-${j}`).disabled = true;
-    }
-    t++;
+  const simbolo = (t % 2 !== 0) ? "X" : "O";
+  griglia[i][j] = simbolo;
+
+  const btn = document.getElementById(`casella-${i}-${j}`);
+  btn.textContent = simbolo;
+  btn.disabled = true;
+
+  t++;
 }
+
+cliccato();
